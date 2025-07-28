c7e41dccb312254195ec0f6262d5ae9695803be9
n1o2h <aythmadnhylt@gmail.com>
2025-07-28 23:02:54 +0100

    rectangkes d'etoiles

---
+++ exercice42.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    printf("***RECTANGLE D'ETOILES***");
+    int l, c, i, j;
+
+    printf("veuillez saisir le nombre de lignes: ");
+    scanf("%d", &l);
+    printf("veuillez saisr le nombre de colonnes: ");
+    scanf("%d", &c);
+
+    for(i = 1; i<=l; i++){
+        for(j = 1; j<= c; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
